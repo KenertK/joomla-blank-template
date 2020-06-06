3eb272e258cf8b13199b9f4545f188a60c5172d8
Kenert <kenertkulu@gmail.com>
2020-06-06 23:53:21 +0300

    Added code to load css and js

--- logic.php
+++ logic.php
@@ -2,9 +2,17 @@
 
 defined('_JEXEC') or die();
 
+use Joomla\CMS\Factory;
+
+$doc   = Factory::getDocument();
+$tpath = $this->baseurl . '/templates/' . $this->template;
+
 // Removes the meta generator tag
 $this->setGenerator(null);
 
 // Removes all the boilerplate JS scripts that come with Joomla templates by default
 $this->_scripts = array();
 unset($this->_script['text/javascript']);
+
+$doc->addStyleSheet($tpath . '/dist/bundle.min.css');
+$doc->addScript($tpath . '/dist/bundle.min.js');
