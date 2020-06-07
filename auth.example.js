7996226cee0cad82aac8fab41524efafe4b3d6a7
Kenert <kenertkulu@gmail.com>
2020-06-07 13:39:38 +0300

    Added auth example

---
+++ auth.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: "FTP_HOST",
+  username: "USERNAME",
+  password: "PASSWORD",
+  port: 21,
+  remotePath: "/your/website/structure",
+  websiteProxy: "http://targetwebsite"
+};
